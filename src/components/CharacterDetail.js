6a4935a0247a25d453effe944d13194f6aaf419a
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-08-09 11:51:33 +0200

    adding css media queries

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -4,6 +4,26 @@ import {Link} from 'react-router-dom';
 import logo from '../images/logo.png';
 
 const CharacterDetail = props => {
+  const getStatusImg = status => {
+    if (status === 'Dead') {
+      return <i class="fas fa-skull" />;
+    } else if (status === 'Alive') {
+      return <i class="fas fa-smile" />;
+    } else {
+      return <i class="fas fa-question-circle" />;
+    }
+  };
+
+  const getSpecieImg = specie => {
+    if (specie === 'Alien') {
+      return <i class="fab fa-reddit-alien" />;
+    } else if (specie === 'Human') {
+      return <i class="fas fa-male" />;
+    } else {
+      return <i class="fas fa-question-circle" />;
+    }
+  };
+
   return (
     <div className="character__detail">
       <img className="logo" src={logo} alt="Rick and Morty" />
@@ -16,8 +36,21 @@ const CharacterDetail = props => {
         />
         <div className="character__detail__content__properties">
           <h2>{props.characterObj.name}</h2>
-          <h3>Status: {props.characterObj.status}</h3>
-          <h3>Spices: {props.characterObj.species}</h3>
+          <h3>
+            Status:
+            {' '}
+            {props.characterObj.status}
+            {' '}
+            {getStatusImg (props.characterObj.status)}
+          </h3>
+          <h3>
+            Spices:
+            {' '}
+            {props.characterObj.species}
+            {' '}
+            {getSpecieImg (props.characterObj.species)}
+            {' '}
+          </h3>
           <h3>Origin: {props.characterObj.origin.name}</h3>
           <h3>Episodes: {props.characterObj.episode.length}</h3>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter} from 'react-router-dom';
 import './stylesheets/index.scss';
-import './stylesheets/reset.scss';
 import App from './components/App';
 
 ReactDOM.render (
