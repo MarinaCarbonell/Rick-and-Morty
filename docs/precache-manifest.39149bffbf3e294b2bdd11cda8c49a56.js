3a13da6c5b34e770080e21518245cf589cc809fe
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-08-09 18:13:00 +0200

    adding docs folder

---
+++ docs/precache-manifest.39149bffbf3e294b2bdd11cda8c49a56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c08f489e01e0540f12e4c4126f2c1a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80eae1906f77d07afc29",
+    "url": "./static/css/main.1b45b49f.chunk.css"
+  },
+  {
+    "revision": "d50c8ea4ff66894045fb",
+    "url": "./static/js/2.648b24ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.648b24ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80eae1906f77d07afc29",
+    "url": "./static/js/main.30756546.chunk.js"
+  },
+  {
+    "revision": "67811d77049c1b7325ec",
+    "url": "./static/js/runtime-main.7c4ac9ee.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
